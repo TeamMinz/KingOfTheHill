36c8729ab6ddd97a886a6a9d117ab48ac5e8c606
Tom Mineo <txm3278@rit.edu>
2020-08-05 03:03:45 -0400

    Add button with no functionality

--- src/components/QueueView/QueueView.js
+++ src/components/QueueView/QueueView.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import '../App/App.css';
-import './QueueView.css'
+import React from "react";
+import "../App/App.css";
+import "./QueueView.css";
+
+const JoinQueue = () => {
+  setButtonAction(LeaveQueue);
+  setButtonText("Leave the Queue");
+
+}
+
+const LeaveQueue = () => {
+  setButtonAction(JoinQueue);
+  setButtonText("Join the Queue");
+  
+}
+
+const [ButtonText, setButtonText] = useState("Join the Queue");
+const [ButtonAction, setButtonAction] = useState(JoinQueue);
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,30 +24,31 @@ export default class App extends React.Component {
 
   render() {
     return (
-    <div className='QueueView'>
-      <div className="Champion">
-      👑 TMinz - 27 wins
+      <div className="QueueView">
+        <div className="Champion">👑 TMinz - 27 wins</div>
+        <hr/>
+        <div className="Queue">
+          <ol>
+            <li>Test1</li>
+            <li>Test2</li>
+            <li>Test3</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+            <li>Test4</li>
+          </ol>
+        </div>
+        <hr/>
+        <div className="Join">
+          <button className="QueueButton" onclick={JoinQueue}>Join the Queue</button>
+        </div>
       </div>
-      <div className="Queue">
-        <ol>
-          <li>Test1</li>
-          <li>Test2</li>
-          <li>Test3</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-        </ol>
-      </div>
-      <div className='Join'>
-
-      </div>
-    </div>);
+    );
   }
 }
