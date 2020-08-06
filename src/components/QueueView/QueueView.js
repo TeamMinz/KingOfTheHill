697abe9e9e2d28bb1c8e9489a94ec4428435cc88
Jack Bass <jeb4571@rit.edu>
2020-08-05 20:50:37 -0400

    Hooked Queue component up to backend functions

--- src/components/QueueView/QueueView.js
+++ src/components/QueueView/QueueView.js
@@ -1,46 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App/App.css";
 import "./QueueView.css";
+import Authentication from '../../util/Authentication/Authentication'
 
 const QueueView = (props) => {
 
+  const twitch = window.Twitch ? window.Twitch.ext : null;
+  const authentication = new Authentication();
+  
   const JoinQueue = () => {
-    setButtonText("Leave the Queue");
-    setButtonAction(() => LeaveQueue);
+    authentication.makeCall("https://localhost:8081/queue/join", "POST").then(resp =>
+    {
+        resp.json().then(body_data => {
+            twitch.rig.log(body_data);
+
+            if (resp.ok) {
+              setButtonText("Leave the Queue");
+              setButtonAction(() => LeaveQueue);
+            }
+        });
+    });
+
+    
   };
 
   const LeaveQueue = () => {
-    setButtonAction(() => JoinQueue);
-    setButtonText("Join the Queue");
+    authentication.makeCall("https://localhost:8081/queue/leave", "POST").then(resp => {
+      resp.json().then(body_data => {
+        twitch.rig.log(body_data);
+
+        if (resp.ok) {
+          setButtonAction(() => JoinQueue);
+          setButtonText("Join the Queue");
+        }
+      });  
+    });
   };
 
+  //state stuff
   const [ButtonText, setButtonText] = useState("Join the Queue");
   const [ButtonAction, setButtonAction] = useState(() => JoinQueue);
+  const [FinishedLoading, setFinishedLoading] = useState(false);
+  const [Queue, setQueue] = useState([]);
 
-  return (
+  //make sure we authorize when the page loads.
+  useEffect(() => {
+    if (twitch) {
+      twitch.onAuthorized((auth) => {
+          authentication.setToken(auth.token, auth.userId);
+          if (!FinishedLoading) {
+            twitch.listen('broadcast', (target, contentType, body) => {
+              let queue = JSON.parse(body);
+              
+              setQueue(queue);
+            });
+
+            authentication.makeCall("https://localhost:8081/queue/get", "GET").then(resp => {
+              if (resp.ok) {
+                resp.json().then(body_data => {
+                  let queue = body_data.queue;
+
+                  if (queue.includes(authentication.getOpaqueId())) {
+                    setButtonText("Leave the Queue");
+                    setButtonAction(() => LeaveQueue);
+                  }
+
+                  setQueue(queue);
+                });
+              }  
+            });
+
+            setFinishedLoading(true);
+          }
+        });
+      }
+  });
+
+  let queueEntries = Queue.map(function (opaqueId, index) {
+    return (<li key={index}>{opaqueId}</li>);
+  });
+
+  return (    
     <div className="QueueView">
       <div className="Champion">👑 TMinz - 27 wins</div>
       <hr />
       <div className="Queue">
         <ol>
-          <li>Test1</li>
-          <li>Test2</li>
-          <li>Test3</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
-          <li>Test4</li>
+          {queueEntries}
         </ol>
       </div>
       <hr />
       <div className="Join">
-        <button className="QueueButton" onClick={ButtonAction}>
+        <button className="QueueButton" onClick={(ButtonAction)}>
           {ButtonText}
         </button>
       </div>
@@ -49,62 +100,3 @@ const QueueView = (props) => {
 };
 
 export default QueueView;
-
-/*
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-    this.state.button = {text: "Join the Queue", handler: this.joinQueue.bind(this)};
-  }
-
-  joinQueue() {
-    //TODO: actually join the queue.
-
-    this.setState({button: {
-      text: "Leave the Queue",
-      handler: this.leaveQueue
-    }});
-  }
-
-  leaveQueue() {
-    //TODO: actually join the queue.
-
-    this.setState({button: {
-      text: "Join the Queue",
-      handler: this.joinQueue
-    }});
-  }
-
-  render() {
-    return (
-      <div className="QueueView">
-        <div className="Champion">👑 TMinz - 27 wins</div>
-        <hr/>
-        <div className="Queue">
-          <ol>
-            <li>Test1</li>
-            <li>Test2</li>
-            <li>Test3</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-            <li>Test4</li>
-          </ol>
-        </div>
-        <hr/>
-        <div className="Join">
-          <button className="QueueButton" onClick={this.state.button.handler.bind(this)}>{this.state.button.text}</button>
-        </div>
-      </div>
-    );
-  }
-}
-*/
