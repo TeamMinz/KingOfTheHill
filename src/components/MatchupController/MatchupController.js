720d6236a51f1a0e29e7e9599d257d03e3dcb996
Jack Bass <jeb4571@rit.edu>
2020-08-16 19:34:57 -0400

    Hooked up Matchup UI to backend

--- src/components/MatchupController/MatchupController.js
+++ src/components/MatchupController/MatchupController.js
@@ -1,23 +1,61 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import Authentication from '../../util/Authentication/Authentication';
 import './MatchupController.css';
 
 const MatchupController = (props) => {
+  const twitch = window.Twitch ? window.Twitch.ext : null;
+  const authentication = new Authentication();
+
   const [CurrentMatchup, setCurrentMatchup] = useState(null);
+  const [FinishedLoading, setFinishedLoading] = useState(false);
 
   const startMatchup = () => {
-    setCurrentMatchup({
-      playerOne: 'pycses',
-      playerTwo: 'tminz',
-    });
+    if (FinishedLoading) {
+      console.log('sending request to start matchup');
+      authentication
+          .makeCall('https://localhost:8081/matchup/start', 'POST')
+          .then((resp) => {
+            if (resp.ok) {
+              console.log(resp);
+
+              resp.json().then((resp) => {
+                console.log('started a matchup: ' + resp);
+                setCurrentMatchup(resp.matchup);
+              });
+            } else {
+              console.log('Error starting matchup.');
+              // Display error
+            }
+          });
+    }
   };
 
   const declareWinner = (winner) => {
-    twitch.ext.log(winner + ' has won!');
-
-    setCurrentMatchup(null);
+    authentication
+        .makeCall('https://localhost:8081/matchup/current/report', 'POST', {winner})
+        .then((resp) => {
+          if (resp.ok) {
+            setCurrentMatchup(null);
+            console.log(`${winner} has been declared the winner.`);
+          } else {
+            console.log('there was an error processing the request.');
+          }
+        });
   };
 
-  if (CurrentMatchup != null) {
+  // make sure we authorize when the page loads.
+  useEffect(() => {
+    if (twitch) {
+      twitch.onAuthorized((auth) => {
+        authentication.setToken(auth.token, auth.userId);
+        if (!FinishedLoading) {
+          setFinishedLoading(true);
+        }
+      });
+    }
+  });
+
+  if (CurrentMatchup != null && FinishedLoading) {
     return (
       <div className="MatchupController">
         Declare a winner:
