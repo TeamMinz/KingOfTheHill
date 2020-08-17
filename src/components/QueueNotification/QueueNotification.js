29a03cf2138600d8fa35af77b7e0a7903f7964a2
Jack Bass <jeb4571@rit.edu>
2020-08-16 20:28:35 -0400

    Basic Framework for "You're Up" notification

---
+++ src/components/QueueNotification/QueueNotification.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+const QueueNotification = (props) => {
+  const [IsVisible, setVisible] = useState(false);
+  const [DisplayMessage, setMessage] = useState("");
+  
+  useEffect(() => {
+    if (twitch) {
+      twitch.onAuthorized((auth) => {
+        authentication.setToken(auth.token, auth.userId);
+        if (!FinishedLoading) {
+          twitch.listen('broadcast', (target, contentType, body) => {
+            const message = JSON.parse(body);
+
+            if (message.messageType == "MESSAGE_YOUREUP") {
+              setMessage(message.message);
+              setVisible(true);
+            }
+          });
+    }
+  });
+
+  if (IsVisible) {
+    return 
+    (<div>
+      {DisplayMessage}
+    </div>);
+  } else {
+    return null;
+  }
+
+  
+};
+
+export default class QueueNotification;
