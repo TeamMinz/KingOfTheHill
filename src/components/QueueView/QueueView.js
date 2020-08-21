be0cbe78a9e71c00630387b043a7cfeb87e463d1
Tom Mineo <txm3278@rit.edu>
2020-08-21 05:10:42 -0400

    Separated QueueView and MatchView

--- src/components/QueueView/QueueView.js
+++ src/components/QueueView/QueueView.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import '../App/App.css';
 import './QueueView.css';
 import Authentication from '../../util/Authentication/Authentication';
-
+import MatchupView from '../MatchupView/MatchupView';
 const QueueView = (_props) => {
   const twitch = window.Twitch ? window.Twitch.ext : null;
   const authentication = new Authentication();
@@ -12,9 +12,11 @@ const QueueView = (_props) => {
   const [ButtonAction, setButtonAction] = useState(() => {});
   const [FinishedLoading, setFinishedLoading] = useState(false);
   const [Queue, setQueue] = useState([]);
-  const [CurrentMatchup, setCurrentMatchup] = useState(null);
 
   // helper functions
+  /**
+   *
+   */
   function fetchQueue() {
     authentication
         .makeCall('https://localhost:8081/queue/get', 'GET')
@@ -32,19 +34,7 @@ const QueueView = (_props) => {
             });
           }
         });
-  };
-
-  function fetchMatchup() {
-    authentication
-        .makeCall('https://localhost:8081/matchup/current/get')
-        .then((resp) => {
-          if (resp.ok) {
-            resp.json().then((resp) => {
-              setCurrentMatchup(resp.matchup);
-            });
-          }
-        });
-  };
+  }
 
   const JoinQueue = () => {
     authentication
@@ -82,7 +72,7 @@ const QueueView = (_props) => {
       if (message.type == 'updateQueue') {
         setQueue(message.message);
       }
-    };
+    }
 
     function handleAuthentication(auth) {
       authentication.setToken(auth.token, auth.userId);
@@ -95,7 +85,6 @@ const QueueView = (_props) => {
 
     function firstTimeSetup() {
       fetchQueue();
-      fetchMatchup();
       setButtonText('Join the queue!');
       setButtonAction(() => JoinQueue);
     }
@@ -113,62 +102,16 @@ const QueueView = (_props) => {
     }
   };
 
-  const MatchupEffect = () => {
-    function handleMessage(_target, _contentType, body) {
-      const message = JSON.parse(body);
-      if (message.type == 'updateMatchup') {
-        setCurrentMatchup(message.message);
-      }
-    };
-
-    /* function handleAuthentication(auth) {
-      authentication.setToken(auth.token, auth.userId);
-
-      if (!FinishedLoading) {
-        setFinishedLoading(true);
-      }
-    }
-
-
-    if (twitch) {
-      twitch.onAuthorized(handleAuthentication);
-    } */ // this is all stuff that we will need when we move this code file
-
-    if (FinishedLoading) {
-      twitch.listen('broadcast', handleMessage);
-
-      return function cleanup() {
-        twitch.unlisten('broadcast', handleMessage);
-      };
-    }
-  };
-
   // called when the component mounts.
   useEffect(QueueEffect, [Queue, FinishedLoading]);
-  useEffect(MatchupEffect, [CurrentMatchup, FinishedLoading]);
 
-
-  const queueEntries = Queue?
-    Queue.map((opaqueId, index) => (<li key={index}>{opaqueId}</li>)):
+  const queueEntries = Queue ?
+    Queue.map((opaqueId, index) => <li key={index}>{opaqueId}</li>) :
     [];
-
-  let headerDiv = null;
-
-  // TODO: we should move this current matchup stuff out of this file.
-  if (CurrentMatchup) {
-    headerDiv = (
-      <div className="Champion">
-        Now Playing: 👑 {CurrentMatchup.champion} (27) v{' '}
-        {CurrentMatchup.challenger}
-      </div>
-    );
-  } else {
-    headerDiv = <div className="Champion">👑 TMinz - 27 wins</div>;
-  }
 
   return (
     <div className="QueueView">
-      {headerDiv}
+      <MatchupView />
       <hr />
       <div className="Queue">
         <ol>{queueEntries}</ol>
