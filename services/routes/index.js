51e39c27cc148a0970629000ac29a87fa0680ede
Jack Bass <jeb4571@rit.edu>
2020-09-14 18:13:38 -0400

    Revert "Added skeleton routes for backend config store"
    This reverts commit cdbbce6f461c701a1011ab4a21eceef25876c4d0.

--- services/routes/index.js
+++ services/routes/index.js
@@ -3,12 +3,10 @@ const router = require('express').Router();
 const queue = require('./queue');
 const matchup = require('./matchup');
 const champion = require('./champion');
-const config = require('./config');
 
 router.use('/queue', queue);
 router.use('/matchup', matchup);
 router.use('/champion', champion);
-router.use('/config', config);
 
 
 router.get('/', (req, res) => res.send('Queue is alive!'));
