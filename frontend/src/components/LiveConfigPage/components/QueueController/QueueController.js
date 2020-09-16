d97d1fda52dfacf513b76b7c556e4f5ca125c49e
Jack Bass <jeb4571@rit.edu>
2020-09-15 22:09:19 -0400

    LiveConfigPage now uses react contexts. Matchup controller now updates when queue closes.

--- frontend/src/components/LiveConfigPage/components/QueueController/QueueController.js
+++ frontend/src/components/LiveConfigPage/components/QueueController/QueueController.js
@@ -1,5 +1,4 @@
-import Authentication from '../../../../util/Authentication/Authentication';
-import React, {useState, useEffect, useRef, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import Collapsible from 'react-collapsible';
 import QueueContext from '../../../../util/QueueContext';
 import '../../../App/App.css';
@@ -13,47 +12,18 @@ import './QueueController.css';
  * @returns {Function} a cleanup function.
  */
 const QueueController = (props) => {
-  const twitch = window.Twitch ? window.Twitch.ext : null;
-  const authProp = useRef(new Authentication());
-  const authentication = authProp.current;
-
   // State stuff.
-  const [FinishedLoading, setFinishedLoading] = useState(false);
   const [ButtonText, setButtonText] = useState('Loading...');
   const [ButtonAction, setButtonAction] = useState(() => {});
-  // const [Queue, setQueue] = useState([]);
-  const [opaqueUserId, setOpaqueID] = useState(null);
-  const [queueIsOpen, setQueueOpen] = useState(true);
 
   // Context
-
   const ctx = useContext(QueueContext);
-
-  console.log(ctx);
-
-  // helper functions
-  /**
-   * Fethches the current queue from the backend.
-   */
-  const fetchQueue = () => {
-    // console.log('fetching queue');
-
-    authentication.makeCall('/queue/get', 'GET').then(function(resp) {
-      if (resp.ok) {
-        resp.json().then((bodyData) => {
-          const queue = bodyData.queue;
-          setQueueOpen(bodyData.isOpen);
-          // setQueue(queue);
-        });
-      }
-    });
-  };
 
   /**
    * Adds the User to the Queue
    */
   const joinQueue = () => {
-    authentication.makeCall('/queue/join', 'POST').then((resp) => {
+    ctx.auth.makeCall('/queue/join', 'POST').then((resp) => {
       resp.json().then((bodyData) => {
         if (resp.ok) {
           setButtonText('Leave the Queue');
@@ -67,7 +37,7 @@ const QueueController = (props) => {
    * Removes the User from the Queue
    */
   const leaveQueue = () => {
-    authentication.makeCall('/queue/leave', 'POST').then((resp) => {
+    ctx.auth.makeCall('/queue/leave', 'POST').then((resp) => {
       resp.json().then((bodyData) => {
         if (resp.ok) {
           setButtonAction(() => joinQueue);
@@ -83,9 +53,7 @@ const QueueController = (props) => {
    * @param {*} opaqueUserId The user to remove from the queue.
    */
   const kickPlayer = (opaqueUserId) => {
-    // console.log(authentication);
-    // console.log('Kicking player ' + opaqueUserId);
-    authentication
+    ctx.auth
         .makeCall('/queue/kick', 'POST', {
           kickTarget: opaqueUserId,
         })
@@ -104,14 +72,13 @@ const QueueController = (props) => {
    * Interfaces with the backend to close the queue.
    */
   const closeQueue = () => {
-    authentication
+    ctx.auth
         .makeCall('/queue/close', 'POST')
         .then((resp) => {
           if (!resp.ok) {
           // TODO: log error.
             return;
           }
-          setQueueOpen(false);
         })
         .catch((err) => {
           console.log(err);
@@ -122,97 +89,26 @@ const QueueController = (props) => {
    * Interfaces with the backend to open the queue.
    */
   const openQueue = () => {
-    authentication
+    ctx.auth
         .makeCall('/queue/open', 'POST')
         .then((resp) => {
           if (!resp.ok) {
           // TODO: log error.
             return;
           }
-          setQueueOpen(true);
         })
         .catch((err) => {
           console.log(err);
         });
   };
 
-  /**
-   * Custom Effect for QueueController
-   * Handles Twitch authentication and pubsubs
-   *
-   * @returns {Function} closing function to stop listening to twitch broadcasts
-   */
-  const QueueEffect = () => {
-    /**
-     * Handles pubsub messages for 'updateQueue'
-     *
-     * @param {string} _target target
-     * @param {string} _contentType content type
-     * @param {object} body message body passed by twitch api.
-     */
-    function handleMessage(_target, _contentType, body) {
-      const message = JSON.parse(body);
-
-      // console.log(message);
-
-      if (message.type == 'updateQueue') {
-        // setQueue(message.message.queue);
-        // setQueueOpen(message.message.status);
-      }
-    }
-
-    /**
-     * Handles authorizing with twitch and running first time setup
-     *
-     * @param {object} auth - twitch authentication information
-     */
-    function handleAuthentication(auth) {
-      authentication.setToken(auth.token, auth.userId);
-      setOpaqueID(authentication.getOpaqueId());
-
-      if (!FinishedLoading) {
-        firstTimeSetup();
-        setFinishedLoading(true);
-      }
-    }
-
-    /**
-     * Sets the Users Opaque and User ID
-     */
-    function firstTimeSetup() {
-      fetchQueue();
-
-      console.log('running first time setup');
-    }
-
-    if (twitch) {
-      twitch.onAuthorized(handleAuthentication);
-
-      if (FinishedLoading) {
-        twitch.onVisibilityChanged((isVisible, context) => {
-          if (isVisible) {
-            twitch.listen('broadcast', handleMessage);
-          }
-        });
-      }
-    }
-
-    if (FinishedLoading) {
-      console.log('listening for broadcast');
-      twitch.listen('broadcast', handleMessage);
-
-      return function cleanup() {
-        twitch.unlisten('broadcast', handleMessage);
-      };
-    }
-  };
-
   // Controls the join / leave button
   useEffect(() => {
-    if (FinishedLoading) {
+    if (ctx.finishedLoading && ctx.queue) {
+      console.log(ctx.queue);
       if (
-        ctx.queue.findIndex(
-            (challenger) => challenger.opaqueUserId == opaqueUserId,
+        ctx.queue.queue.findIndex(
+            (challenger) => challenger.opaqueUserId == ctx.auth.getOpaqueId(),
         ) == -1
       ) {
         setButtonAction(() => joinQueue);
@@ -225,21 +121,21 @@ const QueueController = (props) => {
   }, [ctx.queue]);
 
   // called when the component mounts.
-  useEffect(QueueEffect, [ctx.queue, FinishedLoading]);
 
-  if (FinishedLoading) {
+  if (ctx.finishedLoading && ctx.queue) {
+    console.log(ctx.queue);
     const userEntry = ctx.queue ?
-      ctx.queue.findIndex((challenger) => {
-        return challenger.opaqueUserId == opaqueUserId;
+      ctx.queue.queue.findIndex((challenger) => {
+        return challenger.opaqueUserId == ctx.auth.getOpaqueId();
       }) :
       -1;
 
     const queueEntries = ctx.queue ?
-      ctx.queue.map((challenger, index) => {
+      ctx.queue.queue.map((challenger, index) => {
         return (
           <li key={index}>
             {challenger.displayName}
-            {authentication.isModerator() && (
+            {ctx.auth.isModerator() && (
               <a
                 className="KickButton"
                 style={{float: 'right'}}
@@ -284,15 +180,15 @@ const QueueController = (props) => {
           open={true}
           transitionTime={250}
         >
-          {queueIsOpen && (
+          {ctx.queue.isOpen && (
             <button className="DefaultButton" onClick={closeQueue}>
               Close the Queue
             </button>
           )}
 
-          {queueIsOpen && queueView}
+          {ctx.queue.isOpen && queueView}
 
-          {!queueIsOpen && (
+          {!ctx.queue.isOpen && (
             <button className="DefaultButton" onClick={openQueue}>
               Open the Queue
             </button>
