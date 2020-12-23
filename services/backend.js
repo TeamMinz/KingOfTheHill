86ff1a921c986f7beedbf95a281dd967963de380
Jack Bass <jbass2000@gmail.com>
2020-12-23 15:30:32 -0500

    Removed https from EBS. To be handled by nginx in the future.

--- services/backend.js
+++ services/backend.js
@@ -7,28 +7,6 @@ const config = require('./config.json');
 
 const {authorizeHeader} = require('./util/middleware');
 
-process.env.NODE_ENV = config.mode;
-if (process.env.NODE_ENV == 'development') {
-  // We will be using self signed certs in development.
-  // We need to make sure that we specifically allow that.
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-}
-
-// Load our TLS cert and key.
-
-const TLS_CERT_PATH = config.tls.certPath;
-const TLS_KEY_PATH = config.tls.keyPath;
-const TLS_CA_PATH = config.tls.caPath;
-
-const TLS = {
-  cert: fs.readFileSync(TLS_CERT_PATH),
-  key: fs.readFileSync(TLS_KEY_PATH),
-};
-
-if (process.env.NODE_ENV != 'development') {
-  TLS.ca = fs.readFileSync(TLS_CA_PATH);
-}
-
 const app = express();
 
 app.use(cors());
@@ -37,6 +15,6 @@ app.use(authorizeHeader);
 
 app.use('/', routes);
 
-https.createServer(TLS, app).listen(8081, () => {
+app.listen(8081, () => {
   console.log('EBS now listening.');
 });
