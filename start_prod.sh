6df12be8abbd418d09f41e1b7b2762a05dd9337f
Ubuntu <ubuntu@ip-172-31-4-215.us-east-2.compute.internal>
2021-02-11 16:25:55 +0000

    Initial Watchtower Setup

--- start_prod.sh
+++ start_prod.sh
@@ -1,5 +1,8 @@
 export EXT_SECRET=$(aws secretsmanager get-secret-value --secret-id KOTH-Dev | jq -r '.SecretString | fromjson | .EXT_SECRET');
 export EXT_CLIENT_ID=$(aws secretsmanager get-secret-value --secret-id KOTH-Dev | jq -r '.SecretString | fromjson | .EXT_CLIENT_ID');
 export EXT_OWNER_ID=$(aws secretsmanager get-secret-value --secret-id KOTH-Dev | jq -r '.SecretString | fromjson | .EXT_OWNER_ID');
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account');
+export AWS_DEFAULT_REGION=$(aws configure get region);
+export IMAGE_REPO_NAME="kothservice";
 docker-compose up;
 #docker run koth -e EXT_SECRET="$EXT_SECRET" -e EXT_CLIENT_ID="$EXT_CLIENT_ID" -e EXT_OWNER_ID="$EXT_OWNER_ID";
