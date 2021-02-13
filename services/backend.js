470d38cf4a18fa6aab0227c9f9f71e92369279ee
root <root@ip-172-31-4-215.us-east-2.compute.internal>
2021-02-13 00:39:24 +0000

    Fixed test route, tried to get watchtower to work

--- services/backend.js
+++ services/backend.js
@@ -9,17 +9,16 @@ const {authorizeHeader} = require('./util/middleware');
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(authorizeHeader);
-
-app.use('/', routes);
-
 //Route for testing connectivity.
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
 
+app.use(cors());
+app.use(express.json());
+app.use(authorizeHeader);
+
+app.use('/', routes);
 const HOST = '0.0.0.0';
 const PORT = 8000;
 
