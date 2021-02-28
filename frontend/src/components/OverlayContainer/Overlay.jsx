8fb2baac0ff7f80fd66bffca36b1089df534ddcd
Tom Mineo <txm3278@rit.edu>
2021-02-28 00:06:58 -0500

    Replace drag and resize with one package

--- frontend/src/components/OverlayContainer/Overlay.jsx
+++ frontend/src/components/OverlayContainer/Overlay.jsx
@@ -1,36 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { Resizable } from 're-resizable';
-import Draggable from 'react-draggable';
+import { Rnd } from 'react-rnd';
 import { StyledOverlayContainer } from './Overlay.style';
 import App from '../App/App';
 
 const Overlay = () => {
   const [windowWidth, setWidth] = useState(247);
   const [windowHeight, setHeight] = useState(600);
-
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setWidth(window.innerWidth);
       setHeight(window.innerHeight);
     }
-  });
+  }, []);
 
   return (
-    <Draggable>
-      <Resizable
-        defaultSize={{
-          width: windowWidth,
-          height: windowHeight * 0.65,
-        }}
-        minConstraints={[247, 330]}
-        maxConstraints={[300, 300]}
-        lockAspectRatio
-      >
-        <StyledOverlayContainer>
-          <App format="component" />
-        </StyledOverlayContainer>
-      </Resizable>
-    </Draggable>
+    <Rnd
+      default={{
+        x: 0,
+        y: 50,
+        width: windowWidth,
+        height: windowHeight * 0.65,
+      }}
+      minHeight={330}
+      minWidth={247}
+      maxHeight={500}
+      maxWidth={500}
+      lockAspectRatio
+    >
+      <StyledOverlayContainer>
+        <App format="component" />
+      </StyledOverlayContainer>
+    </Rnd>
   );
 };
 
