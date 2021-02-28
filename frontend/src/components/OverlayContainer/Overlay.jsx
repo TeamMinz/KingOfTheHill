8aca117fa404b8f655b4679098a75d832e663e3a
Tom Mineo <txm3278@rit.edu>
2021-02-27 23:47:19 -0500

    Resizable and Draggable Yarn fixes

--- frontend/src/components/OverlayContainer/Overlay.jsx
+++ frontend/src/components/OverlayContainer/Overlay.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { Resizable } from 're-resizable';
+import Draggable from 'react-draggable';
 import { StyledOverlayContainer } from './Overlay.style';
 import App from '../App/App';
 
-const Overlay = (props) => {
-    return (
+const Overlay = () => {
+  const [windowWidth, setWidth] = useState(247);
+  const [windowHeight, setHeight] = useState(600);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    }
+  });
+
+  return (
+    <Draggable>
+      <Resizable
+        defaultSize={{
+          width: windowWidth,
+          height: windowHeight * 0.65,
+        }}
+        minConstraints={[247, 330]}
+        maxConstraints={[300, 300]}
+        lockAspectRatio
+      >
         <StyledOverlayContainer>
-            <App format="component" />
+          <App format="component" />
         </StyledOverlayContainer>
-      );
-}
+      </Resizable>
+    </Draggable>
+  );
+};
 
 export default Overlay;
--- frontend/src/components/OverlayContainer/Overlay.style.js
+++ frontend/src/components/OverlayContainer/Overlay.style.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
-import { TabPanel } from 'react-tabs';
 
 export const StyledOverlayContainer = styled.div`
-  height: 50vh;
-  width: 20vw;
+  height: 100%;
+  width: 100%;
+  outline: white solid 2px;
 `;
